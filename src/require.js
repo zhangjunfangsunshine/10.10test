dec4377c8b4a08c99e094dcb6d527a3965f628f2
zhangjf <zhangjunfang960828@163.com>
2018-10-10 09:00:04 +0800

    引入模块

---
+++ src/require.js
@@ -0,0 +1,13 @@
+function require() {
+    //引入模块
+    var forArr = reruire(arrs);
+};
+
+function define(arr) {
+    //定义模块
+    var arrs = arr.some(function(item) {
+        return item > 1
+    });
+};
+
+var arr = [1, 2, 3, ]
